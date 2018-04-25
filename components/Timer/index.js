6ee0a475ca1d1f0ad75fd9cfd3b94dfa14385943
llChameleoNll <wonmylover@naver.com>
2018-04-25 21:17:27 +0900

    Add project: Timer (ReactNative)

---
+++ components/Timer/index.js
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from '../../reducer';
+import Timer from './presenter'
+
+function mapStateToProps(state) {
+	const { isPlaying, elapsedTime, timerDuration } = state;
+	return {
+		isPlaying,
+		elapsedTime,
+		timerDuration
+	};
+}
+
+function mapDispatchToProps(dispatch) {
+	return {
+		startTimer: bindActionCreators(actionCreators.startTimer, dispatch),
+		stopTimer: bindActionCreators(actionCreators.stopTimer, dispatch),
+		addSecond: bindActionCreators(actionCreators.addSecond, dispatch)
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Timer);
